8bc7955cab155d3441414a85b1245bd3e01dadce
Michael Law <mlaw135@gmail.com>
2013-05-05 00:10:28 -0400

    key event functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,48 @@
-//call on enter with startTime
+var firstkey=true;
+var startTime=0;
 
-function addComment(startTime) {
+$('#comment-input').keydown(function(event) {
+  if (event.keyCode >= 65 && event.keyCode <= 90) { // if a letter pressed
+  	if(firstkey){
+  		firstkey=false;
+  		startTime = document.getElementById("main-video").currentTime;
+  	}
 
-	var comment = document.getElementById("comment-input").value;
+  }
+});
 
-	var video = document.getElementById("main-video");
-	var endTime = video.currentTime;
+$("#comment-input").keyup(function(e){
+  var code = (e.keyCode ? e.keyCode : e.which);
+  if(code == 13) { //Enter keycode
 
-	var popcorn = Popcorn("#main-video");
-	popcorn.footnote({
-		start: startTime,
-		end: endTime,
-		target: "curAnnotations",
-		text: comment
-	});
-}
+    var comment = document.getElementById("comment-input").value;
+ 	var endTime = document.getElementById("main-video").currentTime;
+
+ 	console.log("Adding comment:"+comment);
+ 	console.log("with StartTime:"+startTime);
+ 	console.log("and endTime:"+endTime);
+
+ 	//TODO: need to fix.
+ 	//add comment to popcorn instance
+	var popcorn = Popcorn( "#main-video" );
+
+ 	popcorn.footnote({
+     start: 0,
+     end: 5,
+     target: "footnotes",
+     text: comment
+   	});
+
+	//TODO:add comment to server
+
+	//TODO:add to annotation-container
+
+ 	//reset stuff
+ 	startTime = 0;
+	document.getElementById("comment-input").value='';
+	firstkey=true;
+  }
+});
 
 function getServer() {
 	$.ajax({
@@ -38,4 +66,9 @@ function getServer() {
 function populatePageWithData() {
 	// THE SERVER HAS SUCCESSFULLY RETURNED THE INFO
 	// DO WORK HERE!
+
+	//add to popcorn instance
+
+	//add to annotation-container
+
 }
