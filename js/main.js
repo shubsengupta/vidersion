d9138d5913ddda98f8f7603ef0666c3bf6ccd8b7
Brett Willemsen <bzmwillemsen@gmail.com>
2013-05-04 23:24:36 -0400

    Added server call

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 //call on enter with startTime
-function addComment(startTime){
+
+function addComment(startTime) {
 
 	var comment = document.getElementById("comment-input").value;
 
- 	var video = document.getElementById("main-video");
- 	var endTime = video.currentTime;
+	var video = document.getElementById("main-video");
+	var endTime = video.currentTime;
 
-	var popcorn = Popcorn( "#main-video" );
- 	popcorn.footnote({
-     start: startTime,
-     end: endTime,
-     target: "curAnnotations",
-     text: comment
-   	});
+	var popcorn = Popcorn("#main-video");
+	popcorn.footnote({
+		start: startTime,
+		end: endTime,
+		target: "curAnnotations",
+		text: comment
+	});
+}
 
+function getServer() {
+	$.ajax({
+		url: "http://vidersion.herokuapp.com/get",
+		type: "GET",
+		dataType: "jsonp",
+		crossDomain: true,
+		jsonp: "callback",
+		// cache: false,
+		jsonpCallback: "JSONPCallback",
+		error: function(xhr, status, error) {
+			console.log("error!");
+			console.dir(error);
+		},
+		success: function(data) {
+			populatePageWithData();
+		}
+	});
+}
 
+function populatePageWithData() {
+	// THE SERVER HAS SUCCESSFULLY RETURNED THE INFO
+	// DO WORK HERE!
 }
