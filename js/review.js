598b35ad257746011702bd6645f9a09118575bc4
Brett Willemsen <bzmwillemsen@gmail.com>
2013-05-05 06:08:01 -0400

    Added review js

--- js/review.js
+++ js/review.js
@@ -164,6 +164,12 @@ function populatePageWithData(currentTime) {
 			content += "<strong>";
 		}
 		content += comment.text;
+		if (comment.state == 'new') {
+			content += '<span class="comment-controls">&nbsp;<i class="icon-ok" id="read' + i + '" onclick="markRead(' + i + ')"></i> | ';
+		} else if (comment.state == 'archived') {
+			content += '<span class="comment-controls">&nbsp;<i class="icon-check" id="read' + i + '" onclick="markRead(' + i + ')"></i> | ';
+		}
+		content += '<i class="icon-trash" id="trash"></i></span>';
 		content += '<span class="time-created" onclick="seekTo(' + comment.start_timecode + ');">';
 		content += 'at ';
 		content += '<a>';
@@ -183,6 +189,47 @@ function populatePageWithData(currentTime) {
 	}
 	$("#annotation-container").empty();
 	$("#annotation-container").append(content);
+}
+
+function markRead(index) {
+	var comment = globalData[index];
+	var newState;
+	if (comment.state == "new") {
+		newState = "archived";
+	} else if (comment.state == "archived") {
+		newState = "new";
+	}
+
+	$.ajax({
+		url: "http://vidersion.herokuapp.com/put",
+		type: "GET",
+		dataType: 'jsonp',
+		data: {
+			start_t: startTime,
+			end_t: Math.floor(document.getElementById("main-video").currentTime),
+			video_id: 1,
+			text: comment,
+			state: newState,
+			comment_id: comment.comment_id
+		},
+		crossDomain: true,
+		error: function(xhr, status, error) {
+			console.log("error!");
+		},
+		success: function(data) {
+			console.log("success!");
+		}
+	});
+
+	if (comment.state == "new") {
+		$('#read' + index).removeClass("icon-ok");
+		$('#read' + index).addClass("icon-check");
+	} else {
+		$('#read' + index).removeClass("icon-check");
+		$('#read' + index).addClass("icon-ok");
+	}
+	comment.state = newState;
+
 }
 
 function seekTo(time) {
