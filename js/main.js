dfa77278af00bbc560a82e36bb227e1ed63ace9c
Brett Willemsen <bzmwillemsen@gmail.com>
2013-05-05 01:42:24 -0400

    Bolding works now

--- js/main.js
+++ js/main.js
@@ -1,52 +1,61 @@
-var firstkey=true;
-var startTime=0;
+var firstkey = true;
+var startTime = 0;
+var currentTime = 0;
+var globalData;
 
 $('#comment-input').keydown(function(event) {
-  if (event.keyCode >= 65 && event.keyCode <= 90) { // if a letter pressed
-  	if(firstkey){
-  		firstkey=false;
-  		startTime = document.getElementById("main-video").currentTime;
-  	}
+	if (event.keyCode >= 65 && event.keyCode <= 90) { // if a letter pressed
+		if (firstkey) {
+			firstkey = false;
+			startTime = document.getElementById("main-video").currentTime;
+		}
 
-  }
+	}
 });
 
-$("#comment-input").keyup(function(e){
-  var code = (e.keyCode ? e.keyCode : e.which);
-  if(code == 13) { //Enter keycode
+$("#comment-input").keyup(function(e) {
+	var code = (e.keyCode ? e.keyCode : e.which);
+	if (code == 13) { //Enter keycode
 
-    var comment = document.getElementById("comment-input").value;
- 	var endTime = document.getElementById("main-video").currentTime;
+		var comment = document.getElementById("comment-input").value;
+		var endTime = document.getElementById("main-video").currentTime;
 
- 	console.log("Adding comment:"+comment);
- 	console.log("with StartTime:"+startTime);
- 	console.log("and endTime:"+endTime);
+		console.log("Adding comment:" + comment);
+		console.log("with StartTime:" + startTime);
+		console.log("and endTime:" + endTime);
 
- 	//TODO: need to fix.
- 	//add comment to popcorn instance
-	var popcorn = Popcorn( "#main-video" );
+		//TODO: need to fix.
+		//add comment to popcorn instance
+		var popcorn = Popcorn("#main-video");
 
- 	popcorn.footnote({
-     start: 0,
-     end: 5,
-     target: "footnotes",
-     text: comment
-   	});
+		popcorn.footnote({
+			start: 0,
+			end: 5,
+			target: "footnotes",
+			text: comment
+		});
 
-	//TODO:add comment to server
+		//TODO:add comment to server
 
-	//TODO:add to annotation-container
+		//TODO:add to annotation-container
 
- 	//reset stuff
- 	startTime = 0;
-	document.getElementById("comment-input").value='';
-	firstkey=true;
-  }
+		//reset stuff
+		startTime = 0;
+		document.getElementById("comment-input").value = '';
+		firstkey = true;
+	}
 });
 
 function getServer() {
+	var popcorn = Popcorn("#main-video");
+	popcorn.on('timeupdate', function(number) {
+		if (Math.floor(this.currentTime()) > currentTime) {
+			currentTime = Math.floor(this.currentTime());
+			populatePageWithData(currentTime);
+		}
+	});
 	$.ajax({
-		url: "http://vidersion.herokuapp.com/get",
+		url: "http://vidersion.herokuapp.com/get?id=1",
 		type: "GET",
 		dataType: "jsonp",
 		crossDomain: true,
@@ -58,17 +67,59 @@ function getServer() {
 			console.dir(error);
 		},
 		success: function(data) {
+			globalData = data;
 			populatePageWithData();
 		}
 	});
 }
 
-function populatePageWithData() {
-	// THE SERVER HAS SUCCESSFULLY RETURNED THE INFO
-	// DO WORK HERE!
+function populatePageWithData(currentTime) {
+	globalData.sort(function(a, b) {
+		if (a.start_timecode < b.start_timecode) {
+			return -1;
+		} else if (a.start_timecode > b.start_timecode) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
 
-	//add to popcorn instance
+	var boldedIndex = 0;
+	for (var j = 0; j < globalData.length; j++) {
+		if (currentTime <= globalData[j].start_timecode) {
+			boldedIndex = j - 1;
+			break;
+		}
+	}
+	if (boldedIndex < 0) {
+		boldedIndex = 0;
+	}
 
-	//add to annotation-container
-
+	var content = "";
+	for (var i = 0; i < globalData.length; i++) {
+		var comment = globalData[i];
+		content += '<div class="annotation old-annotation">';
+		if (i === boldedIndex) {
+			content += "<strong>";
+		}
+		content += comment.text;
+		content += '<span class="time-created">';
+		content += 'at ';
+		content += '<a href="#">';
+		if (comment.start_timecode > 59) {
+			content += Math.floor(comment.start_timecode / 60);
+			content += ":";
+			content += comment.start_timecode % 60;
+		} else if (comment.start_timecode < 10) {
+			content += "0:0" + comment.start_timecode;
+		} else {
+			content += "0:" + comment.start_timecode;
+		}
+		if (i === boldedIndex) {
+			content += "</strong>";
+		}
+		content += '</a></span></div>';
+	}
+	$("#annotation-container").empty();
+	$("#annotation-container").append(content);
 }
